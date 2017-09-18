aa7332c0440e5b4879507f35062a233abb46a9c1
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-09-18 19:36:41 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+
+
+export default class EyesOnMe extends React.Component {
+ 
+
+  Good = () => {
+ 	console.log('Hey! Eyes on me!');
+ 	}
+  EyesOnMe = () => {
+    console.log('Good!', 'The console output is not the expected string.');
+  }
+ 
+  render() {
+    return (
+      <button onBlur= {this.Good} onFocus={this.EyesOnMe}></button>
+    );
+  }
+}
